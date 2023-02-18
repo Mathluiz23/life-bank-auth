41e4715a5e6a5e1d36eae5f092ea8c3368b035ab
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2023-02-18 16:14:09 -0300

    Implementando teste de verificação para o token

--- src/life-bank-auth.Test/TestTokenGenerator.cs
+++ src/life-bank-auth.Test/TestTokenGenerator.cs
@@ -5,23 +5,29 @@ namespace LifeBankAuth.Test;
 
 public class TestTokenGenerator
 {
-    /// <summary>
-    /// Test function that validates if Token is being generated, if it is not returning null or empty
-    /// </summary>
-    [Theory(DisplayName = "Teste para TokenGenerator em que token não é nulo")]
-    [InlineData("Mayara", false, CurrencyEnum.Real)]
-    public void TestTokenGeneratorSuccess(string name, bool isCompany, CurrencyEnum currency)
-    {
-        throw new NotImplementedException();
-    }
+  /// <summary>
+  /// Test function that validates if Token is being generated, if it is not returning null or empty
+  /// </summary>
+  [Theory(DisplayName = "Teste para TokenGenerator em que token não é nulo")]
+  [InlineData("Mayara", false, CurrencyEnum.Real)]
+  public void TestTokenGeneratorSuccess(string name, bool isCompany, CurrencyEnum currency)
+  {
+    var client = new Client(name, isCompany, currency);
+    var token = TokenGenerator.Generate(client);
+    Assert.NotNull(token);
+    Assert.NotEmpty(token);
+  }
 
-    /// <summary>
-    /// Test function that validates if Token is being generated according to JWT methodology, having 3 keys.
-    /// </summary>
-    [Theory(DisplayName = "Teste para TokenGenerator em que token JWT possui 3 partes")]
-    [InlineData("Mayara", false, CurrencyEnum.Real)]
-    public void TestTokenGeneratorKeysSuccess(string name, bool isCompany, CurrencyEnum currency)
-    {
-        throw new NotImplementedException();
-    }
+  /// <summary>
+  /// Test function that validates if Token is being generated according to JWT methodology, having 3 keys.
+  /// </summary>
+  [Theory(DisplayName = "Teste para TokenGenerator em que token JWT possui 3 partes")]
+  [InlineData("Mayara", false, CurrencyEnum.Real)]
+  public void TestTokenGeneratorKeysSuccess(string name, bool isCompany, CurrencyEnum currency)
+  {
+    var client = new Client(name, isCompany, currency);
+    var token = TokenGenerator.Generate(client);
+    var tokenParts = token.Split('.');
+    Assert.Equal(3, tokenParts.Length);
+  }
 }
